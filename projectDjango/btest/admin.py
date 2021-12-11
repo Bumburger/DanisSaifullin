1a79bd2fa25603d6d7b9697b64f9f268f06257ee
Bumburger <93050869+Bumburger@users.noreply.github.com>
2021-12-11 03:17:57 +0300

    Add files via upload

---
+++ projectDjango/btest/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Bb
+from .models import Client
+from .models import Order
+
+
+
+admin.site.register(Bb)
+admin.site.register(Client)
+admin.site.register(Order)
